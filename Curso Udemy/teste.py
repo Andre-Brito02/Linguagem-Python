52a3ddb5fdf98488b302830c8eb5ec32c4cdfbfb
Andre-Brito02 <andrebritosantos002@gmail.com>
2025-01-28 10:35:45 -0300

    Organizando os exercicios em uma pasta e trazendo novos conhecimentos

--- Curso Udemy/teste.py
+++ Curso Udemy/teste.py
@@ -1,11 +1,27 @@
-nums = [1, 2, 3, 4, 5, 6]
+# nums = [1, 2, 3, 4, 5, 6]
 
-for i in range(len(nums)):
-    x = 0
-    while(x <= i):
-        print(nums[x], end=" ")
-        x += 1
-    print()
+# for i in range(len(nums)):
+#     x = 0
+#     while(x <= i):
+#         print(nums[x], end=" ")
+#         x += 1
+#     print()
 
 # texto = 'Olá mundo'
 # print(texto[::-1])# Inverter um texto
+
+# def numeros_divisiveis_por_7(numbers):
+#     numeros_divisiveis_por_7 = [num for num in numbers if num %7 == 0 ]
+#     print(numeros_divisiveis_por_7)
+
+# def numeros_que_contem_3(numbers):
+#     numeros_que_contem_3 = [str(num) for num in numbers if str(3) in num]
+#     print(numeros_que_contem_3)
+
+# numeros_divisiveis_por_7(numbers = [x for x in range(1, 1000)])
+# numeros_que_contem_3(numbers = [str(x) for x in range(1, 1000)])
+
+# frase = 'sandjsndkjadsk sjadksajdklsajkd sljd sakljdsa kdjsak djaskd sakdja skl'
+# quantidade_de_espacos = [letra for letra in frase if letra == ' ']
+# print(len(quantidade_de_espacos))
+
